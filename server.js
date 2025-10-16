42cefbdd310cd6145c7aeffc76931c7224d3fd44
mwichabe <mwichabecollins@gmail.com>
2025-10-16 20:22:35 +0300

    All updated changes

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 5000;
 // --- MIDDLEWARE CONFIGURATION ---
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://one-man-botique.pages.dev",
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
